04b1bc3190ff5f6d72df37a616d423173f85b8c2
calumacutt <calumacutt@gmail.com>
2022-01-05 15:38:22 +1000

    more q4

--- haskell/q4/main.hs
+++ haskell/q4/main.hs
@@ -1,15 +1,17 @@
 import Data.List.Split (splitOn, chunksOf)
+import Data.List
+
 
 -- Reading Input
 
 readInput :: [String] -> ([Int], [[[Int]]]) 
-readInput (draw : gap : cards) = (readDraw draw, readCards cards)
+readInput (draw : cards) = (readDraw draw, readCards cards)
 
 readDraw :: String -> [Int]
 readDraw draw = map (read::String->Int) (splitOn "," draw)
 
 readCards :: [String] -> [[[Int]]]
-readCards cards = map (readCardRows . init) (chunksOf 6 cards)
+readCards cards = map (readCardRows . tail) (chunksOf 6 cards)
 
 readCardRows :: [String] -> [[Int]]
 readCardRows = map readCardRow
@@ -17,9 +19,32 @@ readCardRows = map readCardRow
 readCardRow :: String -> [Int]
 readCardRow row = map read (filter (/= "") (splitOn " " row))
 
+-- 
 
+markNumberOnCards :: Int -> [[[Int]]] -> [[[Int]]]
+markNumberOnCards draw = (map . map) (markNumberOnRow draw)
+
+-- markNumberOnCard :: Int -> [[Int]] -> [[Int]]
+-- markNumberOnCard draw = map (markNumberOnRow draw)
+
+markNumberOnRow :: Int -> [Int] -> [Int]
+markNumberOnRow draw row = [if x == draw then -x else x | x <- row]
+
+--
+
+checkCardForBingo :: [[Int]] -> Bool
+checkCardForBingo card = any (all (<0)) (card ++ transpose card)
+
+sumNonNegatives :: [[Int]] -> Int
+sumNonNegatives card = sum (map sumNonNegativesRow card)
+
+sumNonNegativesRow :: [Int] -> Int
+sumNonNegativesRow row = sum (filter (>=0) row)
+
+--
 
 main :: IO ()
 main = do
     listOfStrs <- lines <$> readFile "input-small.txt"
-    print (let (draw, cards) = readInput listOfStrs in cards)
+    -- print (let (draw, cards) = readInput listOfStrs in markNumberOnCards 12 cards)
+    print $ sumNonNegatives [[1,1,1],[1,-1,1],[1,1,1]]
